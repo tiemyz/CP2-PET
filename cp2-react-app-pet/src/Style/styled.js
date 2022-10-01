514e4e3f0fe2daea09b5d03be2a4bec6c818a1cb
tiemyz <rm95145@fiap.com.br>
2022-10-01 18:57:03 -0300

    Styled atualizado.
    Co-authored-by: Felipe Galvão <FlipOwz@users.noreply.github.com>

--- cp2-react-app-pet/src/Style/styled.js
+++ cp2-react-app-pet/src/Style/styled.js
@@ -1,39 +1,26 @@
 import styled from 'styled-components'
 
-export const DivLista = styled.div`
+export const DivListaPet = styled.div`
     width: 100%;
     min-height: 85vh;
-    background-color: #ffb;
-    padding: 20px;
-    border: 2px solid #ccc;
-    display: flex; flex-wrap: wrap;
+    background-color: #e0bcdd;
+    padding: 50px;
+    display: flex; 
+    flex-wrap: wrap;
     justify-content: space-around;
 `
 
-export const DivTarefa = styled.div`       
-    background-color : #ffb;
-    border : 2px solid #333;
-    box-shadow : 5px 5px 5px #333;
+export const DivTarefaPet = styled.div`       
+    background-color : #f0ddee;
     padding: 2vh 2vw;
     text-align:center;
-    width:50vw;
-    height:20vh;
-    margin:5vh 30vw;
+    width:30vw;
+    height:35vh;
+    border-radius: 20px;
     h2,p{
             padding-bottom:1vh
         }
     button{
             position: absolute; top: 10px; right: 10px;
-    }
-`
-
-export const BtnClose = styled.button`
-    max-width: 20vw;
-    max-height: 20vh;
-    border: 2px solid #00f000;
-    border-radius: 5px;
-    &:hover{
-        background-color: #0000ff;
-        color: #fff;
     }
 `
