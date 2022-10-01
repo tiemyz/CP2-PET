3103b583596a7de3f01ae71eff30c3bdf7776078
tiemyz <rm95145@fiap.com.br>
2022-10-01 19:05:26 -0300

    Props Pet atualizado.
    Co-authored-by: VSeiji <vSeiji@users.noreply.github.com>

--- cp2-react-app-pet/src/Components/Pet/Pet.jsx
+++ cp2-react-app-pet/src/Components/Pet/Pet.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
-import {DivTarefa} from '../../Style/styled'
+import {DivTarefaPet} from '../../Style/styled'
 import {FaTrashAlt as X} from "react-icons/fa";
+import './pets.css'
 
 export default function Pet(props) {
+
     return(
-        <DivTarefa>
-                <p><b>Nome :</b> {props.nome}</p>
-                <p><b>Idade :</b> {props.idade}</p>
-                <p><b>Raça :</b> {props.raca}</p>
-                <p><b>Tamanho :</b> {props.tamanho}</p>
-                <p><b>Nome do dono :</b> {props.nomedodono}</p>
-                <p><b>Telefone do dono :</b> {props.telefonedodono}</p>
-                <p><b>Imagem do pet :</b> {props.imagemdopet}</p>
-                <p><b>Observações :</b> {props.observacoes}</p>
+        
+        <DivTarefaPet>
+            <div class="info-pet">
+                <p>Nome do Pet: {props.nomePet}</p>
+                <p>Idade: {props.idadePet}</p>
+                <p>Raça: {props.racaPet}</p>
+                <p>Tamanho: {props.tamanhoPet}</p>
+                <p>Nome do dono: {props.nomeDono}</p>
+                <p>Telefone do dono: {props.telefoneDono}</p>
+                <p>Imagem do pet : {props.imagemdopet}</p>
+                <p>Observações : {props.observacoesPet}</p>
                 <button onClick={props.remove}><X/></button>
-        </DivTarefa>
+            </div>
+        </DivTarefaPet>
     )
 }
 
